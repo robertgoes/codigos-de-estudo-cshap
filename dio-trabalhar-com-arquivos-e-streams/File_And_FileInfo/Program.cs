14f17e987100f8b017af4dbe2fae142dc3f18a99
Robert <robert.goes@hotmail.com>
2022-12-08 10:55:12 -0300

    Criar Arquivo

---
+++ dio-trabalhar-com-arquivos-e-streams/File_And_FileInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+//Definir o caminho(diretório do arquivo).
+var diretorioDoArquivoTexto = Path.Combine(Environment.CurrentDirectory, "texte.txt");
+
+//Criar um arquivo de texto, passando por parâmetro o path(diretório).
+//Declaramos o using para usar o método flush automaticamente.
+using var arquivoTexto = File.CreateText(diretorioDoArquivoTexto);
+
+//Através do método WriteLine, podemos passar o conteúdo do texto, por paramêtro.
+arquivoTexto.WriteLine("Está é a linha 1");
+arquivoTexto.WriteLine("Está é a linha 2");
